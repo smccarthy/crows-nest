a9ac93477027d57aba130579c4901f4eee43c40c
Lei Zhu <lzhu@walmart.com>
2016-12-01 18:51:07 -0800

    fix unhandled rejection error

--- test/tunnel.test.js
+++ test/tunnel.test.js
@@ -1,6 +1,7 @@
 import { STATE, Tunnel } from "../tunnel";
 import Promise from "bluebird";
 import chai from "chai";
+import chaiAsPromise from "chai-as-promised";
 
 import logger from "../logger";
 
@@ -14,7 +15,10 @@ logger.output = {
 
 import _ from "lodash";
 
+chai.use(chaiAsPromise);
+
 const expect = chai.expect;
+const assert = chai.assert;
 
 describe("Tunnel", () => {
   let sauceConnectLauncherMock = null;
@@ -58,20 +62,13 @@ describe("Tunnel", () => {
   describe("Start tunnel", function () {
     this.timeout(60000);
 
-    it("Straight succeed", (done) => {
-      t.start()
-        .then(() => {
-          expect(t.state).to.equal(STATE.RUNNING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+    it("Straight succeed", () => {
+      return t.start()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.RUNNING))
+        .catch(err => assert(false, "Tunnel failed in launching up"));
     });
 
-    it("Succeed with retry", (done) => {
+    it("Succeed with retry", () => {
       let count = 0;
 
       t.sauceConnectLauncher = (options, cb) => {
@@ -83,45 +80,27 @@ describe("Tunnel", () => {
         }
       };
 
-      t.start()
-        .then(() => {
-          expect(t.state).to.equal(STATE.RUNNING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.start()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.RUNNING))
+        .catch(err => assert(false, "Tunnel failed in launching up with retry"));
     });
 
-    it("Fail after 10 retries", (done) => {
-      t.sauceConnectLauncher = (options, cb) => { cb(new Error("fake error"), null); };
-
-      t.start()
-        .then(() => {
-          expect(true).to.equal(false);
-          done();
-        })
-        .catch((err) => {
-          expect(t.state).to.equal(STATE.IDLE);
-          expect(true).to.equal(true);
-          done();
-        });
+    it("Fail after 10 retries", () => {
+      t.sauceConnectLauncher = (options, cb) => { cb(new Error("fake_err"), null); };
+      process.on("unhandledRejection", function (reason, promise) {
+        // See Promise.onPossiblyUnhandledRejection for parameter documentation
+        console.log(reason, promise)
+      });
+      return t.start()
+        .then(() => assert(false, "Tunnel succeeded in launching up"))
+        .catch((err) => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE));
     });
 
-    it("Skip starting if state isn't IDLE", (done) => {
+    it("Skip starting if state isn't IDLE", () => {
       t.state = STATE.STARTING;
 
-      t.start()
-        .then(() => {
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.start()
+        .catch(err => assert(false, "Tunnel succeeded in launching up with state = IDLE"));
     });
 
   });
@@ -129,152 +108,83 @@ describe("Tunnel", () => {
   describe("Stop tunnel", function () {
     this.timeout(60000);
 
-    beforeEach((done) => {
-      t.start()
-        .then(() => {
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+    beforeEach(() => {
+      return t.start()
+        .catch(err => assert(false, "Tunnel succeeded in launching up"));
     });
 
-    it("Straight success", (done) => {
-      t.stop()
-        .then(() => {
-          expect(t.state).to.equal(STATE.IDLE);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+    it("Straight success", () => {
+      return t.stop()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+        .catch(err => assert(false, "Tunnel doesn't in stop"));
     });
 
-    it("Return success if tunnel died", (done) => {
+    it("Return success if tunnel died", () => {
       t.tunnelProcess = null;
 
-      t.stop()
-        .then(() => {
-          expect(t.state).to.equal(STATE.IDLE);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.stop()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+        .catch(err => assert(false, "Tunnel is still alive"));
     });
 
-    it("Skip stopping if state is IDLE", (done) => {
+    it("Skip stopping if state is IDLE", () => {
       t.state = STATE.IDLE;
 
-      t.stop()
-        .then(() => {
-          expect(t.state).to.equal(STATE.IDLE);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.stop()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+        .catch(err => assert(false, "Tunnel is still alive"));
     });
 
-    it("Skip stopping if state is STOPPING", (done) => {
+    it("Skip stopping if state is STOPPING", () => {
       t.state = STATE.STOPPING;
 
-      t.stop()
-        .then(() => {
-          expect(t.state).to.equal(STATE.STOPPING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.stop()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.STOPPING))
+        .catch(err => assert(false, "Tunnel is still alive"));
     });
 
-    it("Skip stopping if state is QUITTING", (done) => {
+    it("Skip stopping if state is QUITTING", () => {
       t.state = STATE.QUITTING;
 
-      t.stop()
-        .then(() => {
-          expect(t.state).to.equal(STATE.QUITTING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.stop()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.QUITTING))
+        .catch(err => assert(false, "Tunnel is still alive"));
     });
 
     describe("Kill tunnel", () => {
-      it("Kill successfully", (done) => {
+      it("Kill successfully", () => {
         t.treeKill = (pid, signal, cb) => { cb() };
 
-        t.kill(true)
-          .then(() => {
-            expect(t.state).to.equal(STATE.IDLE);
-            expect(true).to.equal(true);
-            done();
-          })
-          .catch((err) => {
-            expect(true).to.equal(false);
-            done();
-          });
+        return t.kill(true)
+          .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+          .catch(err => assert(false, "Tunnel is killed"));
       });
 
-      it("Fail in kill", (done) => {
+      it("Fail in kill", () => {
         t.treeKill = (pid, signal, cb) => { cb("fake_err") };
 
-        t.kill(true)
-          .then(() => {
-            expect(true).to.equal(false);
-            done();
-          })
-          .catch((err) => {
-            expect(t.state).to.equal(STATE.RUNNING);
-            expect(true).to.equal(true);
-            done();
-          });
+        return t.kill(true)
+          .then(() => assert(false, "Tunnel is killed"))
+          .catch(err => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.RUNNING));
       });
 
-      it("Do nothing if stop isn't timing out", (done) => {
+      it("Do nothing if stop isn't timing out", () => {
         t.treeKill = (pid, signal, cb) => { cb("fake_err") };
         t.state = STATE.IDLE;
 
-        t.kill()
-          .then(() => {
-            expect(t.state).to.equal(STATE.IDLE);
-            expect(true).to.equal(true);
-            done();
-          })
-          .catch((err) => {
-            expect(true).to.equal(false);
-            done();
-          });
+        return t.kill()
+          .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+          .catch(err => assert(false, "Tunnel is killed"));
       });
 
-      it("Do nothing if tunnelProcess is null", (done) => {
+      it("Do nothing if tunnelProcess is null", () => {
         t.treeKill = (pid, signal, cb) => { cb("fake_err") };
         t.state = STATE.IDLE;
         t.tunnelProcess = null;
 
-        t.kill()
-          .then(() => {
-            expect(t.state).to.equal(STATE.IDLE);
-            expect(true).to.equal(true);
-            done();
-          })
-          .catch((err) => {
-            expect(true).to.equal(false);
-            done();
-          });
+        return t.kill()
+          .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.IDLE))
+          .catch(err => assert(false, "Tunnel is killed"));
       });
     });
   });
@@ -282,58 +192,31 @@ describe("Tunnel", () => {
   describe("Restart tunnel", function () {
     this.timeout(60000);
 
-    beforeEach((done) => {
-      t.start()
-        .then(() => {
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+    beforeEach(() => {
+      return t.start()
+        .catch(err => assert(false, "Tunnel isn't started"));
     });
 
-    it("Straight success", (done) => {
-      t.restart()
-        .then(() => {
-          expect(t.state).to.equal(STATE.RUNNING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+    it("Straight success", () => {
+      return t.restart()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.RUNNING))
+        .catch(err => assert(false, "Tunnel isn't restarted"));
     });
 
-    it("Do nothing if state is STARTING", (done)=>{
+    it("Do nothing if state is STARTING", () => {
       t.state = STATE.STARTING;
 
-      t.restart()
-        .then(() => {
-          expect(t.state).to.equal(STATE.STARTING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.restart()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.STARTING))
+        .catch(err => assert(false, "Tunnel is restarted"));
     });
 
-    it("Do nothing if state is STOPPING", (done)=>{
+    it("Do nothing if state is STOPPING", () => {
       t.state = STATE.STOPPING;
 
-      t.restart()
-        .then(() => {
-          expect(t.state).to.equal(STATE.STOPPING);
-          expect(true).to.equal(true);
-          done();
-        })
-        .catch((err) => {
-          expect(true).to.equal(false);
-          done();
-        });
+      return t.restart()
+        .then(() => expect(Promise.resolve(t.state)).to.eventually.equal(STATE.STOPPING))
+        .catch(err => assert(false, "Tunnel is restarted"));
     });
 
   });
